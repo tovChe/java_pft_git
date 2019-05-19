a864999ba3abfaac6196ec0bd92791bc9992c1c5
Aleksandr Chechulin <9615085535@mail.ru>
2019-05-19 22:58:37 +0300

    add v1.0 Point

--- sandbox/src/main/java/ru/jft/first/Point.java
+++ sandbox/src/main/java/ru/jft/first/Point.java
@@ -1,4 +1,16 @@
 package ru.jft.first;
 
 public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getDistance(Point p2) {
+    return Math.sqrt(Math.pow(p2.x - this.x, 2) + Math.pow(p2.y - this.y, 2));
+  }
 }
--- sandbox/src/main/java/ru/jft/first/first.java
+++ sandbox/src/main/java/ru/jft/first/first.java
@@ -8,5 +8,12 @@ public class First {
 
         System.out.println(1.0 / 2.0);
         System.out.println(Math.sqrt(4));
+
+        Point p1 = new Point(10, 10);
+        Point p2 = new Point(50, 50);
+
+        System.out.println("Расстояние между точками = " + p1.getDistance(p2));
     }
+
+
 }
