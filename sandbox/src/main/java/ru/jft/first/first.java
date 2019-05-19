c1bb807191920d5cf3b8b9f4ae188f72293ec3ff
Aleksandr Chechulin <9615085535@mail.ru>
2019-05-19 17:51:55 +0300

    Create first.java

---
+++ sandbox/src/main/java/ru/jft/first/first.java
@@ -0,0 +1,12 @@
+package ru.jft.first;
+
+public class first {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+
+        System.out.println(1.0 / 2.0);
+        System.out.println(Math.sqrt(4));
+    }
+}
