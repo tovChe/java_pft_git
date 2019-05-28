5d25e0698612cb2e6b9f89bdbbff7a2aac097e83
Aleksandr Chechulin <9615085535@mail.ru>
2019-05-28 22:53:56 +0300

    create ApplicationManager

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/TestBase.java
@@ -1,118 +1,20 @@
 package ru.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
 
-  private WebDriver wd;
+  protected final ApplicationManager app = new ApplicationManager();
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  protected void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-
-  }
-
-  protected void fillGroupForm() {
-    GroupData gdata = new GroupData("Lopar", "Zaverda", "TestGroup Name");
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).sendKeys(gdata.textFooter);
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).sendKeys(gdata.header);
-    wd.findElement(By.name("group_name")).sendKeys(gdata.groupName);
-  }
-
-  protected void createGroup() {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
-  }
-
-  protected void submitForm() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  protected void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
+    app.init();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    logout();
-    wd.quit();
+    app.stop();
   }
 
-  protected void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  protected void returnHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  protected void addNewPerson() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  protected void fillPersonForm(PersonData personData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(personData.getPersonName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(personData.getPersonLastName());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(personData.getTelNumber());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(personData.getEmail());
-  }
-
-  protected void deleteGroup() {
-    wd.findElement(By.name("delete")).click();
-  }
-
-  protected void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
-  }
-
-  protected void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
 }
