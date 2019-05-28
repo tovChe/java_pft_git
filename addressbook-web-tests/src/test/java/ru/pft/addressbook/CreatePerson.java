8453e2ac9752adaac6d93e2ce1f7e9b77577a66f
Aleksandr Chechulin <9615085535@mail.ru>
2019-05-28 20:32:51 +0300

    add CreatePerson test

---
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/CreatePerson.java
@@ -0,0 +1,93 @@
+package ru.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class CreatePerson {
+  private WebDriver wd;
+
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testCreatePerson() throws Exception {
+    wd.get("http://localhost/addressbook/edit.php");
+    login("admin", "secret");
+    fillPersonForm(new PersonData("Tester", "Testovoy", "89999999999", "test@test.com"));
+    submitForm();
+    returnHomePage();
+
+  }
+
+  private void returnHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitForm() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillPersonForm(PersonData personData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(personData.getPersonName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(personData.getPersonLastName());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(personData.getTelNumber());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(personData.getEmail());
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    logout();
+    wd.quit();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/PersonData.java
@@ -0,0 +1,31 @@
+package ru.pft.addressbook;
+
+public class PersonData {
+  private final String personName;
+  private final String personLastName;
+  private final String telNumber;
+  private final String email;
+
+  public PersonData(String personName, String personLastName, String telNumber, String email) {
+    this.personName = personName;
+    this.personLastName = personLastName;
+    this.telNumber = telNumber;
+    this.email = email;
+  }
+
+  public String getPersonName() {
+    return personName;
+  }
+
+  public String getPersonLastName() {
+    return personLastName;
+  }
+
+  public String getTelNumber() {
+    return telNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
