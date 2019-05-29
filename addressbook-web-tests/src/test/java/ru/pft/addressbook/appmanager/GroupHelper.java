d3a30b725ed04dc07bbecaa1f312ecad837a2eb1
Aleksandr Chechulin <9615085535@mail.ru>
2019-05-29 22:12:38 +0300

    refactor v3 with Helpers

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/GroupHelper.java
@@ -4,32 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.pft.addressbook.model.GroupData;
 
-public class GroupHelper {
-
-  private WebDriver wd;
+public class GroupHelper extends HelperBase {
 
   public GroupHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void createGroup() {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    click(By.linkText("groups"));
+    click(By.name("new"));
   }
 
   public void deleteGroup() {
-    wd.findElement(By.name("delete")).click();
+    click(By.name("delete"));
   }
 
   public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+    click(By.name("selected[]"));
   }
 
   public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.textFooter);
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.header);
-    wd.findElement(By.name("group_name")).sendKeys(groupData.groupName);
+    type(By.name("group_footer"), groupData.textFooter);
+    type(By.name("group_header"), groupData.header);
+    type(By.name("group_name"), groupData.groupName);
   }
+
 }
--- addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/PersonHelper.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/PersonHelper.java
@@ -4,30 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.pft.addressbook.model.PersonData;
 
-public class PersonHelper {
-
-  private WebDriver wd;
+public class PersonHelper extends HelperBase {
 
   public PersonHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void addNewPerson() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 
   public void fillPersonForm(PersonData personData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(personData.getPersonName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(personData.getPersonLastName());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(personData.getTelNumber());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(personData.getEmail());
+
+    type(By.name("firstname"), personData.getPersonName());
+    type(By.name("lastname"), personData.getPersonLastName());
+    type(By.name("mobile"), personData.getTelNumber());
+    type(By.name("email"), personData.getEmail());
+
   }
 }
