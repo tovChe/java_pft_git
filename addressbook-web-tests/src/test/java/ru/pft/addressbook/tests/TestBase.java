d9c3a289782586c1b34509a424e8259e427e3a23
Aleksandr Chechulin <9615085535@mail.ru>
2019-07-01 22:52:06 +0300

    add local.properties and run test suites

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/TestBase.java
@@ -5,17 +5,19 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
-  public void setUp() throws Exception {
+  public void setUp() throws IOException {
     app.init();
   }
 
   @AfterSuite
-  public void tearDown() throws Exception {
+  public void tearDown() {
     app.stop();
   }
 
